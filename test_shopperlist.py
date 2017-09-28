bf8da89b485669078af9ff7390ea9acd2480e79c
Maina Erick <erickmaina35@gmail.com>
2017-09-28 15:30:01 +0300

    change file name to test_shopperlist

---
+++ test_shopperlist.py
@@ -0,0 +1,12 @@
+import unittest
+from shopperlist import ShopperList
+
+class TestList(unittest.TestCase):
+    """
+    Perform unit testing for the shopperlist class
+    """
+    def setUp(self):
+        """The setUp method before doing the tests"""
+        self.newList = ShopperList()
+
+        
